57e5cb081daccba84319cde2e4404009644770bb
timstoneberg <tstoneberg@luc.edu>
2020-02-11 13:04:36 -0600

    Updated to be far less noobish.

---
+++ SeedGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SeedGenerator {
+    class Program {
+        static void Main(string[] args) {
+            SeedGenerator.Seed seed = SeedGenerator.Generate(8);
+
+            Console.WriteLine("The seed is: " + seed.StringValue);
+            Console.WriteLine("The int seed is: " + seed.NumericValue.ToString());
+        }
+    }
+}
--- SeedGenerator/SeedGenerator.cs
+++ SeedGenerator/SeedGenerator.cs
@@ -1,76 +1,69 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace SeedGenerator {
+
+    /*  =====================================================================
+         *  Generates a random seed, to be used to create random variables.
+         *
+         *  Usage Example:
+         *      >> SeedGenerator.Seed seed = SeedGenerator.Generate(8);
+         *  ================================================================= */
     public static class SeedGenerator {
-        private static string charactersAvailable = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        private static Random r = new System.Random();
 
-        public static string GenerateRandomSeed(int length) {
-            string seed = String.Empty;
-            char[] characterSeed1 = CreateCharacterSeed(length / 2);
-            char[] characterSeed2 = CreateCharacterSeed(length / 2);
+        public struct Seed {
+            public string StringValue { get; set; }
+            public int NumericValue { get; set; }
+        }
 
-            seed = new string(characterSeed1).ToUpper();
-            seed += new string(characterSeed2).ToUpper();
+        public static char[] charactersAvailable = {
+            'A','B','C','D','E',
+            'F','G','H','I','J',
+            'K','L','M','N','O',
+            'P','Q','R','S','T',
+            'U','V','W','X','Y',
+            'Z','0','1','2','3',
+            '4','5','6','7','8',
+            '9'
+        };
 
+        public static Random random = new System.Random();
+
+        /*  =================================================================
+         *  Generates a random alphanumeric seed based on a provided length.
+         *  
+         *  Properties:
+         *      >> length:  How many characters to make the seed
+         *  ================================================================= */
+        public static Seed Generate(int length) {
+            Debug.Assert(length > 0 && length < 9, "Seeds must be between 1 and 8 characters!");
+
+            char[] c = new char[length];
+            for (int i = 0; i < length; i++) {
+                c[i] = charactersAvailable[random.Next(0, charactersAvailable.Length - 1)];
+            }
+
+            Seed seed = new Seed();
+            seed.StringValue = new string(c);
+            seed.NumericValue = ToInt(seed.StringValue);
             return seed;
         }
 
-        public static int ConvertToNumeric(string stringSeed) {
-            string part1 = string.Empty;
-            string part2 = string.Empty;
-            int valueOffset = 1;
+        /*  =================================================================
+         *  Converts the alphanumeric seed to a long, prior to passing to
+         *  a random number generator.
+         *  
+         *  Properties:
+         *      >> seed:  The alphanumeric seed
+         *  ================================================================= */
+        private static int ToInt(string seed) {
+            string s = String.Empty;
 
-            string stringSeed1 = stringSeed.Substring(0, stringSeed.Length / 2);
-            string stringSeed2 = stringSeed.Substring(stringSeed.Length / 2, stringSeed.Length / 2);
-
-            char[] charSeed1 = CreateCharacterSeed(stringSeed1);
-            char[] charSeed2 = CreateCharacterSeed(stringSeed2);
-
-            for (int i = 0; i < charSeed1.Length; i++) {
-                part1 += (valueOffset + charactersAvailable.IndexOf(charSeed1[i])).ToString();
+            foreach (char c in seed) {
+                s += (Array.IndexOf(charactersAvailable, c) % 10).ToString();
             }
-            for (int i = 0; i < charSeed2.Length; i++) {
-                part2 += (valueOffset + charactersAvailable.IndexOf(charSeed2[i])).ToString();
-            }
-
-            int result1 = Int32.Parse(part1);
-            int result2 = Int32.Parse(part2);
-
-            return (result1 * 2) + (result2 * 2);
-        }
-
-        private static char[] CreateCharacterSeed(int seedLength) {
-            var result = new char[seedLength];
-
-            for (int i = 0; i < result.Length; i++) {
-                result[i] = charactersAvailable[r.Next(charactersAvailable.Length)];
-            }
-
-            return result;
-        }
-
-        private static char[] CreateCharacterSeed(string seed) {
-            var result = new char[seed.Length];
-
-            for (int i = 0; i < result.Length; i++) {
-                result[i] = seed[i];
-            }
-
-            return result;
-        }
-
-        private static int ConvertToNumeric(char[] charSeed) {
-            string s = string.Empty;
-            int valueOffset = 1;
-
-            for (int i = 0; i < charSeed.Length; i++) {
-                s += (valueOffset + charactersAvailable.IndexOf(charSeed[i])).ToString();
-            }
-
-            int result = Int32.Parse(s);
-            result *= 2;
-            return result;
+            return Int32.Parse(s);
         }
     }
 }
